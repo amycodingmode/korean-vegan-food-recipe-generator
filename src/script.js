35b3ca46745e7952cf23c067fcde01d95d869829
amycodingmode <giantmotion@gmail.com>
2025-06-21 12:35:30 -0400

    Added Typewritter plugin

--- src/script.js
+++ src/script.js
@@ -1,7 +1,16 @@
 function generateRecipe(event) {
-  event.preventDeault();
+  event.preventDefault();
 
-  alert("Generating");
+  //let recipeElement = document.querySelector("#recipe");
+  //recipeElement.innerHTML = "Korean Vegan Recipe";
+  //don't need this anymore because we added Typewriter effect
+
+  new Typewriter("#recipe", {
+    strings: "Korean Vegan Recipe",
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
