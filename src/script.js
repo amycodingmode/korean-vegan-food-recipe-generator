2fd8d0ac3a44a45fbe4562b3c42f7aa45b04e8ae
amycodingmode <giantmotion@gmail.com>
2025-06-23 18:43:04 -0400

    Improved the User Experience

--- src/script.js
+++ src/script.js
@@ -18,9 +18,13 @@ function generateRecipe(event) {
   let instructionInput = document.querySelector("#user-instructions");
   let apiKey = "bd87c3f1a268o0e7ca45515816tf700b";
   let context =
-    "You're the most creative, famous, and talented vegan chef. You are the vegan expert. Your mission is to generate a simple and easy vegan recipe in a basic HTML format and separate each line with a <br />. Start with title in h2 format. For title of Ingredients and Instructions, use h3 format. For ingredients, use CSS List style: inside. For instructions, use style with numbers. Do not repeat the context. Do not show html in id=recipe. Make sure to follow the user instructions.";
+    "You're the most creative, famous, and talented vegan chef. You are the vegan expert. Your mission is to generate a simple and easy vegan recipe in a basic HTML format and separate each line with a <br />. Start with title in h2 format. For title of Ingredients and Instructions, use h3 format. For ingredients, Must use CSS List style: inside. For instructions, use style with numbers. Do not repeat the context. Do not show html in id=recipe. Make sure to follow the user instructions.";
   let prompt = `User instructions: Generate a Korean vegan dish recipe about ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="blink">⏳ Generating a Korean vegan recipe about ${instructionInput.value} for you 😊</div>`;
 
   console.log("Generating a recipe");
   console.log(`Prompt: ${prompt}`);
